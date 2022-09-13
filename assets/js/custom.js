fcabeccb684173e72c2668fdb7bb49ad3a8d2fd1
Md Sahil <mohdsahilparsoli@gmail.com>
2022-09-13 11:21:16 +0530

    fansathiprojects

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+/* swiper js */
+var swiper = new Swiper(".swiperabout", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView:3,
+    loop:true,
+    // autoplay:{
+    //     delay:500
+    // }
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop:true
+  });
